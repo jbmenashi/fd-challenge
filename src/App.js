59fb47fbe41096d8cc5566689eb75bc99b01b449
Jacob <jbmenashi@gmail.com>
2019-04-08 13:58:26 -0400

    fetched plays

--- src/App.js
+++ src/App.js
@@ -9,46 +9,62 @@ import PlayByPlay from './components/PlayByPlay'
 
 const mapStateToProps = state => {
    return {
-      test: state.test
+      plays: state.plays
+   }
+}
+
+const mapDispatchToProps = dispatch => {
+   return {
+      fetchPlays: (plays) => dispatch({type: "FETCH_PLAYS", payload: plays})
    }
 }
 
 class App extends Component {
-  render() {
-    return (
-      <Fragment>
-         <Navbar/>
-         <Grid padded>
-            <Grid.Row stretched>
-               <Grid.Column width={11} id="left-column">
-                  <Grid stretched columns={1}>
-                     <Grid.Column width={16}>
-                        <Segment>1</Segment>
-                     </Grid.Column>
-                  </Grid>
-                  <Grid stretched columns={2}>
-                     <Grid.Column width={8}>
-                        <Segment>
-                           <PlayByPlay/>
-                        </Segment>
-                     </Grid.Column>
-                     <Grid.Column width={8}>
-                        <Segment>
-                           <Stats/>
-                        </Segment>          
-                     </Grid.Column>
-                  </Grid>     
-               </Grid.Column>
-               <Grid.Column width={5} id="right-column">
-                  <Segment>
-                     <Scoring />
-                  </Segment>
-               </Grid.Column>
-            </Grid.Row>
-         </Grid>
-      </Fragment>
-    );
-  }
+
+   componentDidMount() {
+      fetch("https://my-json-server.typicode.com/fanduel/moneyball-fe-challenge-data/db")
+      .then(res => res.json())
+      .then(data => {
+         this.props.fetchPlays(data.plays)
+      })
+   }
+
+
+   render() {
+      return (
+         <Fragment>
+            <Navbar/>
+            <Grid padded>
+               <Grid.Row stretched>
+                  <Grid.Column width={11} id="left-column">
+                     <Grid stretched columns={1}>
+                        <Grid.Column width={16}>
+                           <Segment>1</Segment>
+                        </Grid.Column>
+                     </Grid>
+                     <Grid stretched columns={2}>
+                        <Grid.Column width={8}>
+                           <Segment>
+                              <PlayByPlay/>
+                           </Segment>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                           <Segment>
+                              <Stats/>
+                           </Segment>          
+                        </Grid.Column>
+                     </Grid>     
+                  </Grid.Column>
+                  <Grid.Column width={5} id="right-column">
+                     <Segment>
+                        <Scoring />
+                     </Segment>
+                  </Grid.Column>
+               </Grid.Row>
+            </Grid>
+         </Fragment>
+      );
+   }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,14 @@
 const initialState = {
-   funds: 1000000.00
+   funds: 1000000.00,
+   plays: []
 }
 
 export default (state = initialState, action) => {
    switch (action.type) {
       case 'ADD_FUNDS':
          return {...state, funds: state.funds += 1000000}
+      case 'FETCH_PLAYS':
+         return {...state, plays: action.payload}
       default:
          return state
    }
