d26a634cd83f4a7757b0a88b410c1a6a5e2cfc74
Jacob <jbmenashi@gmail.com>
2019-04-07 23:57:45 -0400

    navbar work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { Menu } from 'semantic-ui-react';
 import './App.css';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
 
 const mapStateToProps = state => {
    return {
@@ -12,22 +12,24 @@ const mapStateToProps = state => {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React!!
-          </a>
-        </header>
-      </div>
+      <Fragment>
+         <Menu size='massive' id="navbar">
+            <Menu.Item>
+               <img src="https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/032018/untitled-1_341.png?FbJ4ipi9S5_57j4d8bj_0xIeiwDUP1TD&itok=pVUrNAy2" />
+            </Menu.Item>
+            <Menu.Item name="lobby" id="navbar-item" />
+            <Menu.Item name="upcoming" id="navbar-item" />
+            <Menu.Item name="live" id="navbar-item" />
+            <Menu.Item name="history" id="navbar-item" />
+            <Menu.Menu position="right">
+               <Menu.Item name="help" id="navbar-item" />
+               <Menu.Item name="username" id="navbar-item" />
+               <Menu.Item name="funds" id="navbar-item" />
+            </Menu.Menu>
+            
+         </Menu>
+      
+      </Fragment>
     );
   }
 }
