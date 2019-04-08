d1c9366f04357db0d433692e1b2d4d650ecb126d
Jacob <jbmenashi@gmail.com>
2019-04-07 22:59:25 -0400

    implement redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
+
+const mapStateToProps = state => {
+   return {
+      test: state.test
+   }
+}
 
 class App extends Component {
   render() {
@@ -25,4 +32,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
---
+++ src/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+   test: 1
+}
+
+export default (state = initialState, action) => {
+   switch (action.type) {
+      default:
+         return state
+   }
+}
