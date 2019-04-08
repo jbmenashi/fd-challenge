97f749aee74a80718ae8cefede951b10da56ed94
Jacob <jbmenashi@gmail.com>
2019-04-08 10:51:57 -0400

    navbar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Menu, Icon, Button } from 'semantic-ui-react';
 import './App.css';
 import {connect} from 'react-redux';
+import Navbar from './Navbar'
 
 const mapStateToProps = state => {
    return {
@@ -13,26 +14,7 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-         <Menu size='massive' id="navbar">
-            <Menu.Item>
-               <img src="https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/032018/untitled-1_341.png?FbJ4ipi9S5_57j4d8bj_0xIeiwDUP1TD&itok=pVUrNAy2" />
-            </Menu.Item>
-            <Menu.Item name="lobby" id="navbar-item" />
-            <Menu.Item name="upcoming" id="navbar-item" />
-            <Menu.Item name="live" id="navbar-item">Live<Icon name="angle down"/></Menu.Item>
-            <Menu.Item name="history" id="navbar-item" />
-            <Menu.Menu position="right">
-               <Menu.Item name="help" id="navbar-item">Help<Icon name="angle down"/></Menu.Item>
-               <Menu.Item name="username" id="navbar-item"><Icon name="user circle"/>User<Icon name="angle down"/></Menu.Item>
-               <Menu.Item name="funds" id="navbar-item">
-                  $1,000,000.00  <br/>
-                  BALANCE  
-                  <Button color="green">Add Funds</Button>
-               </Menu.Item>
-            </Menu.Menu>
-            
-         </Menu>
-      
+         <Navbar/>
       </Fragment>
     );
   }
