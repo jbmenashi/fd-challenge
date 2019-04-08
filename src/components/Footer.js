a9e952d9b3c181710d3693433f7ae6c8e4115946
Jacob <jbmenashi@gmail.com>
2019-04-08 17:15:29 -0400

    footer sports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Menu, Icon, Button } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 
 class Footer extends Component {
    render() {
@@ -7,7 +7,7 @@ class Footer extends Component {
          <Fragment>
             <div id="footer">
                <div id="footer-sports">
-                  <Menu secondary id="footer-menu">
+                  <Menu secondary size="small" id="footer-menu">
                      <Menu.Item active name='NBA'/>
                      <Menu.Item name='MLB'/>
                      <Menu.Item name='NCAAF'/>
