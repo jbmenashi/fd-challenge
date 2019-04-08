ddb4e395fb88cb40f26a3ee7c50ad45c24cb8502
Jacob <jbmenashi@gmail.com>
2019-04-08 15:49:38 -0400

    play by play complete

--- src/components/Play.js
+++ src/components/Play.js
@@ -8,6 +8,13 @@ class Play extends Component {
             <div id="play-image">
                <img src={require(`../images/${this.props.scoring_player}.png`)} width={90} height={75}/>
             </div>
+            <div id="play-info">
+               <div>{this.props.time_left} {this.props.quarter}th</div>
+               <div id="play-desc">{this.props.description}</div>
+            </div>
+            <div id="play-score">
+               {this.props.away_score}-{this.props.home_score}, GS
+            </div>
          </div>
       );
    }
