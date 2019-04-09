79c2374fe3b7cdcf4aa1cd35978e4569f1b29040
Jacob <jbmenashi@gmail.com>
2019-04-08 22:34:53 -0400

    more progress on infobox

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -24,7 +24,11 @@ class InfoBox extends Component {
                      <p>96</p>
                      </Grid.Column>
                      <Grid.Column width={4} id="infobox-game-info">
-                     {this.props.gameInfo.time_left}
+                        <div>
+                           <span style={{fontWeight: 'bold', fontSize: '1.3em'}}>{this.props.gameInfo.time_left} {this.props.gameInfo.quarter}th</span><br/><br/>
+                           {this.props.gameInfo.arena} <br/>
+                           {this.props.gameInfo.location}
+                        </div>
                      </Grid.Column>
                      <Grid.Column width={1} id="infobox-home-score">
                      <p>81</p>
@@ -41,7 +45,15 @@ class InfoBox extends Component {
                      </Grid.Column>
                   </Grid.Row>
                   <Grid.Row id="infobox-row-3">
-                     Row 3
+                     <Grid.Column width={4}>
+                        A
+                     </Grid.Column>
+                     <Grid.Column width={8}>
+                        <img src={require(`../images/shot_chart.png`)} alt="" />
+                     </Grid.Column>
+                     <Grid.Column width={4}>
+                        C
+                     </Grid.Column>
                   </Grid.Row>
                </Grid>
             </div>
